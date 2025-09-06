d5f4f3bb5f1e7177bc1e787d2f3463c7a69611da
gunjan <gunjankhadatkar23@gmail.com>
2025-09-06 20:08:07 +0530

    post deletion feature added

--- app/(tabs)/bookmarks.tsx
+++ app/(tabs)/bookmarks.tsx
@@ -1,57 +1,61 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router'
-import { Ionicons } from '@expo/vector-icons';
-import { COLORS } from '@/constants/theme';
+import { Loader } from "@/components/Loader";
+import { COLORS } from "@/constants/theme";
+import { api } from "@/convex/_generated/api";
+import { styles } from "@/styles/feed.styles";
+import { useQuery } from "convex/react";
+import { Image } from "expo-image";
+import { View, Text, ScrollView } from "react-native";
 
-export default function TabLayout() {
-    return (
-        <Tabs
-            screenOptions={{
-                tabBarShowLabel: false,
-                headerShown: false,
-                tabBarActiveTintColor: COLORS.primary,
-                tabBarInactiveTintColor: COLORS.grey,
-                tabBarStyle: {
-                    backgroundColor: "black",
-                    borderTopWidth: 0,
-                    position: "absolute",
-                    elevation: 0,
-                    height: 40,
-                    paddingBottom: 8,
-                },
-            }} >
-            <Tabs.Screen
-                name='index'
-                options={{
-                    tabBarIcon: ({ color, size }) => <Ionicons name="home" color={color} size={size} />
-                }}
-            />
-            <Tabs.Screen
-                name='bookmarks'
-                options={{
-                    tabBarIcon: ({ color, size }) => <Ionicons name="bookmark" color={color} size={size} />
-                }}
-            />
+export default function Bookmarks() {
+  const bookmarkedPosts = useQuery(api.bookmarks.getBookmarkedPosts);
 
-            <Tabs.Screen
-                name='create'
-                options={{
-                    tabBarIcon: ({ color, size }) => <Ionicons name="add-circle" color={COLORS.primary} size={size} />
-                }}
-            />
-            <Tabs.Screen
-                name='notifications'
-                options={{
-                    tabBarIcon: ({ color, size }) => <Ionicons name="heart" color={color} size={size} />
-                }}
-            />
-            <Tabs.Screen
-                name='profile'
-                options={{
-                    tabBarIcon: ({ color, size }) => <Ionicons name="person-circle" color={color} size={size} />
-                }}
-            />
-        </Tabs>
-    )
+  if (bookmarkedPosts === undefined) return <Loader />;
+  if (bookmarkedPosts.length === 0) return <NoBookmarksFound />;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Bookmarks</Text>
+      </View>
+
+      {/* POSTS */}
+      <ScrollView
+        contentContainerStyle={{
+          padding: 8,
+          flexDirection: "row",
+          flexWrap: "wrap",
+        }}
+      >
+        {bookmarkedPosts.map((post) => {
+          if (!post) return null;
+          return (
+            <View key={post._id} style={{ width: "33.33%", padding: 1 }}>
+              <Image
+                source={post.imageUrl}
+                style={{ width: "100%", aspectRatio: 1 }}
+                contentFit="cover"
+                transition={200}
+                cachePolicy="memory-disk"
+              />
+            </View>
+          );
+        })}
+      </ScrollView>
+    </View>
+  );
+}
+
+function NoBookmarksFound() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: COLORS.background,
+      }}
+    >
+      <Text style={{ color: COLORS.primary, fontSize: 22 }}>No bookmarked posts yet</Text>
+    </View>
+  );
 }
--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -13,6 +13,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as bookmarks from "../bookmarks.js";
 import type * as comments from "../comments.js";
 import type * as http from "../http.js";
 import type * as post from "../post.js";
@@ -27,6 +28,7 @@ import type * as users from "../users.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  bookmarks: typeof bookmarks;
   comments: typeof comments;
   http: typeof http;
   post: typeof post;
